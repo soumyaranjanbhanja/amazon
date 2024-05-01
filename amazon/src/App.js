83d7a5e6b634d7c1874ef64a5a64f5e9669b231f
soumyaranjanbhanja <soumyaranjanb@gmail.com>
2024-05-01 13:06:40 +0530

    v1

---
+++ amazon/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <div className="a_main"></div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
